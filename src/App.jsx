d94bc0338b68b2551b10123e76749117d4a02a28
yousof-ali <yousofali293@gmail.com>
2025-03-03 13:05:14 +0600

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <h1 className='text-5xl'>Vite + React</h1>
+      <button className="btn btn-neutral">Neutral</button>
+<button className="btn btn-primary">Primary</button>
+<button className="btn btn-secondary">Secondary</button>
+<button className="btn btn-accent">Accent</button>
+<button className="btn btn-info">Info</button>
+<button className="btn btn-success">Success</button>
+<button className="btn btn-warning">Warning</button>
+<button className="btn btn-error">Error</button>
+       
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Routes/Router.jsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import {
+  createBrowserRouter,
+} from "react-router-dom";
+import Root from "../Layout/Root";
+import Home from "../Pages/Home";
+
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root></Root>,
+      children:[
+        {
+            path:'/',
+            element:<Home></Home>
+        }
+      ]
+    },
+  ]);
+
+export default router;  
